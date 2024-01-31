d07b6c48e224e562b0e4ab0938e6f331ca064d3c
rushRobBlob <robbie.rush@ymail.com>
2024-01-31 22:33:01 +0000

    more typescript shenanigans

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ var message = 'Hello, TypeScript!';
 var heading = document.createElement('h1');
 heading.textContent = message;
 document.body.appendChild(heading);
+var dogTup = ['hey', 'hi', 'hello'];
--- app.ts
+++ app.ts
@@ -3,4 +3,4 @@ let message: string = 'Hello, TypeScript!';
 let heading = document.createElement('h1');
 heading.textContent = message;
 document.body.appendChild(heading);
-console.log('heeeyyy');
+let dogTup: string[] = ['hey', 'hi', 'hello'];
