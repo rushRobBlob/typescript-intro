d587bdf1dcf85e2037580779ea69ac25b7797389
rushRobBlob <robbie.rush@ymail.com>
2024-02-05 15:38:14 +0000

    enums

---
+++ enums2.js
@@ -0,0 +1,14 @@
+var petsOnSale = 'chinchilla';
+var orderArray = [
+    ['rat', 2],
+    ['chinchilla', 1],
+    ['hamster', 2],
+    ['chinchilla', 50]
+];
+var Pets;
+(function (Pets) {
+    Pets["Hamster"] = "HAMSTER";
+    Pets["Rat"] = "RAT";
+    Pets["Chinchilla"] = "CHINCHILLA";
+    Pets["Tarantula"] = "TARANTULA";
+})(Pets || (Pets = {}));
---
+++ enums2.ts
@@ -0,0 +1,16 @@
+let petsOnSale = 'chinchilla';
+let orderArray = [
+    ['rat', 2],
+    ['chinchilla', 1],
+    ['hamster', 2],
+    ['chinchilla', 50]
+];
+
+enum Pets {
+    Hamster = 'HAMSTER',
+    Rat = 'RAT',
+    Chinchilla = 'CHINCHILLA',
+    Tarantula = 'TARANTULA'
+}
+
+let petsOnSaleTS: Pets = Pets.Chinchilla;
